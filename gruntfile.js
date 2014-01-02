a58df447e95327854fe756b6fbd105814b7798d7
Andrew Liebchen <aliebchen@gofdn.com>
2014-01-01 23:14:30 -0500

    Blog setup

--- gruntfile.js
+++ gruntfile.js
@@ -11,34 +11,6 @@ module.exports = function(grunt) {
       }
     },
 
-    concat: {
-      dist: {
-        src: [
-          'javascripts/vendor/*.js', // All JS in the libs folder
-          'javascripts/application.js'  // This specific file
-        ],
-        dest: 'javascripts/application.js',
-      }
-    },
-
-    uglify: {
-      build: {
-        src: 'javascripts/application.js',
-        dest: 'javascripts/application.min.js'
-      }
-    },
-
-    imagemin: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: ['**/*.{png,jpg,gif}'],
-          dest: 'images/build/'
-        }]
-      }
-    },
-
     sass: {
       dist: {
         options: {
@@ -55,14 +27,6 @@ module.exports = function(grunt) {
         files: ['javascripts/*.coffee'],
         tasks: 'coffee'
       },
-
-      scripts: {
-        files: ['javascripts/*.js'],
-        tasks: ['concat', 'uglify'],
-        options: {
-            spawn: false,
-        },
-      },
       css: {
           files: ['stylesheets/scss/*.scss'],
           tasks: ['sass'],
@@ -76,12 +40,9 @@ module.exports = function(grunt) {
 
   // 3. Where we tell Grunt we plan to use this plug-in.
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['coffee', 'concat', 'uglify', 'imagemin', 'sass', 'watch']);
+  grunt.registerTask('default', ['coffee', 'sass', 'watch']);
 };
